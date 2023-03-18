195aa5fdd97980018a62235d65b9192ac8ddee4b
Rashamba <rashamba528@gmail.com>
2023-03-18 18:28:38 +0300

    Add pytest

--- stepik/registration_required.py
+++ stepik/registration_required.py
@@ -7,8 +7,6 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Ваш код, который заполняет обязательные поля
-
 
     element1 = browser.find_element(By.XPATH, "//input[@placeholder='Input your first name']")
     element1.send_keys("Test")
@@ -30,13 +28,10 @@ try:
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
 
     
